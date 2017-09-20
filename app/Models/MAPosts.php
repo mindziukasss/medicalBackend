501542a090307a686e85804e2afff64943190dfb
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-09-20 14:14:37 +0300

    added function user to posts model

--- app/Models/MAPosts.php
+++ app/Models/MAPosts.php
@@ -7,4 +7,12 @@ class MAPosts extends CoreModel
     protected $table = 'ma_posts';
 
     protected $fillable = ['id', 'user_id', 'title', 'text'];
+
+    protected  $with = ['user'];
+
+    public function user()
+    {
+        return $this->hasOne(MAUsers::class, 'id','user_id' );
+    }
+
 }
